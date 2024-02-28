12f0a89f45a27517056b5d439f3eb89dd8dd0a6c
imrandell <99010684+imrandell@users.noreply.github.com>
2024-02-29 01:16:30 +0530

    Update Ek_Machli_Cheat.py

--- Ek_Machli_Cheat.py
+++ Ek_Machli_Cheat.py
@@ -8,88 +8,103 @@ def generate_random_player():
     out = False  # Default value for is_out is False
     return {'player_name': name, "is_out": out}
 
-def main():
+def announce_winner(players):
+    # Print the winner
+    print(f"\nCongratulations, {players[0]['player_name']}! You are the winner!")
+    return True
+
+def main(min_iteration):
     num_players = int(input("\nEnter the number of players: "))
     
     enter_player_names= input("\nDo you want to enter player names? (1/0): ")
     
     if enter_player_names == "1":
-        players = [input(f"\nEnter the name of player {i+1}: ") for i in range(num_players)]
+        players = [{'player_name': input(f"\nEnter the name of player {i+1}: "), 'is_out': False} for i in range(num_players)]
     else:
         players = [generate_random_player() for _ in range(num_players)]
         print(f"\nGenerated random player names: {print([player['player_name'] for player in players])}")
     
-    my_position = int(input('Enter your position (1,2,3...)'))
-    my_name = players[my_position-1]['player_name']
-    print(f'Your name is {my_name}')
-
     play_or_cheat = input("\nDo you want to Play(1) or Cheat(0)? (1/0): ")
 
-    play_game(players,play_or_cheat,my_position)
+    play_game(players,play_or_cheat,min_iteration)
 
-def play_game(players,play_or_cheat,my_position):
-    your_name = players[my_position-1]['player_name']
-    right_answer = 1
+def play_game(players,play_or_cheat,min_iteration):
+    round = 0
+    right_answer_number = 1
     expected_answer_loop=1
     word_reduction_required=1
-    while len(players) > 1:
+    active_player_count = len([player for player in players if not player['is_out']]) 
+    fair_play = play_or_cheat=="1"
+    if (not fair_play): #cheat
+        my_position = int(input('Enter your position (1,2,3...)'))
+        your_name = players[my_position-1]['player_name']
+        print(f'Your name is {your_name}')
+
+    while active_player_count> 1 and min_iteration>0:
+        round+=1
+        print (f'\n---------Round = {round}, Active players = {active_player_count}---------')
+        min_iteration-=1
+       
         for player in players:
+            # current_player_index  = next((i for i, p in enumerate(players) if p == player), None)
+            # print (current_player_index)
             current_player_name = player['player_name']
             if player["is_out"]: 
                 continue
-            if (current_player_name == your_name and play_or_cheat != "1"):
-                no_of_wrong_answers_user_input=0
-                no_of_wrong_answers_user_input = input('How many players gave wrong answer?')
-                no_of_wrong_answers = int(no_of_wrong_answers_user_input) if no_of_wrong_answers_user_input != '3' and no_of_wrong_answers_user_input.isdigit() else 0
-                if (no_of_wrong_answers>0):
-                    for i in range(no_of_wrong_answers):
-                        player_index_to_terminate =  players.index(player)-1
-                        player_to_terminate =  players[player_index_to_terminate]
-                        print(f'{player_to_terminate} is out')
-                        players.pop(player_index_to_terminate)
-                        right_answer = 1
-                        expected_answer_loop=1
-                        word_reduction_required=1
             
-            answer = str(right_answer)
-            
-            if(play_or_cheat=="1"):
-                # Prompt the player for their input
-                answer = input(f"\n{current_player_name}, enter the word's number (1 for '{expected_answer_loop} Machli', 2 for 'Pani me gayi', 3 for 'Chhapak'): ").strip().capitalize()
-            else:
+            prefix = str(expected_answer_loop)+" " if right_answer_number == 1 else ""#increase fish count
+
+            if (not fair_play): #cheat
                 if (current_player_name == your_name):
-                    prefix = expected_answer_loop if right_answer == 1 else ""
-                    print (f"\nPlayer {current_player_name} Your Answer is {prefix} {WORDS[right_answer-1]} ,  answer_loop = {expected_answer_loop}, word_reduction_required = {word_reduction_required} ")
+                    answer = str(right_answer_number)
+                    is_wrong="0"
+                    print (f"\n{current_player_name}: Your Choice should be {prefix}{WORDS[right_answer_number-1].upper()}.\n")
+                    # print (f", answer_loop = {expected_answer_loop}, word_reduction_required = {word_reduction_required}")
+                else:
+                    is_wrong = input(f"\t{current_player_name}\'s choice shoud be {prefix}{WORDS[right_answer_number-1].upper()}, Is he/she wrong (1/0)?")
+                    is_wrong_int = int(is_wrong) if is_wrong.isdigit() else 0
+                    if (is_wrong_int<1):
+                        answer = str(right_answer_number) #right answer
+                    else:
+                        answer = str(right_answer_number+1) #wrong answer`
+            else: #fair play
+                # Prompt the player for their input
+                answer = input(f"{current_player_name}, select your choice (1 for '{expected_answer_loop} Machli', 2 for 'Pani me gayi', 3 for 'Chhapak'): ").strip().capitalize()
 
             # Check if the player's input matches the expected word or its index
-            if answer != str(right_answer):
-                print(f"\nOops! {current_player_name} said the wrong word. right answer is {right_answer},  {current_player_name} is out!")
+            if answer != str(right_answer_number):
+                print(f"Oops! {current_player_name} said the wrong word. right answer is {right_answer_number},  {current_player_name} is out!")
                 player["is_out"]=True
-                right_answer = 1
+                active_player_count -=1
+                right_answer_number = 1
                 expected_answer_loop=1
                 word_reduction_required=1
+                # current_player_index = [player['player_name'] for player in players].index(current_player_name) 
+                # print(f'Start new roun3d with {active_player_count}, player {players[current_player_index+1]['player_name']} to make first call')
             else:
-                if(play_or_cheat=="1"):
-                    print ('Right Ansser')      
+                if (fair_play): #cheat
+                    print (f'Right Answer : {prefix}{WORDS[right_answer_number-1].upper()}')      
+
                 if (word_reduction_required>0):
                     word_reduction_required-=1
                 
                 if (word_reduction_required==0):
-                    right_answer += 1
+                    right_answer_number += 1
                     word_reduction_required = expected_answer_loop
 
-                if (right_answer > len(WORDS)):
+                if (right_answer_number > len(WORDS)):
                     #print (f"Because {answer_number} > {len(WORDS)}, thus reseting answer_number to 1")
-                    right_answer = 1
+                    right_answer_number = 1
                     # Increase round multiplier after each complete cycle of 'Ek Machli', 'Pani me gayi', and 'Chhapak'
                     expected_answer_loop += 1
                     word_reduction_required=expected_answer_loop
                     #print (f"Repeate word {expected_answer_loop} times")
+                
+                if (active_player_count<=1):
+                    break
 
-    # Print the winner
-    print(f"\nCongratulations, {players[0]['player_name']}! You are the winner!")
-    # Restart the game with remaining players
-    main()
+    announce_winner(players)
+    main(100)
 
 if __name__ == "__main__":
-    main()
+    main(100)
